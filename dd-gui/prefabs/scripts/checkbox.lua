56ec41aef55e3ca0f8ab50e98b821ce141c6d8d2
Nils Gustafsson <ngustafsson90@gmail.com>
2024-09-20 08:52:35 +0200

    update

--- dd-gui/prefabs/scripts/checkbox.lua
+++ dd-gui/prefabs/scripts/checkbox.lua
@@ -16,7 +16,7 @@ function M.clearCheckbox (self, node)
 end
 	
 
-function M.checkbox(self, action_id, action, node, enabled, text)
+function M.checkbox(self, action_id, action, node, enabled, standard_value, text)
 	-- Load nodes
 	local bgNode = gui.get_node(node .. "/bg")
 	local checkNode = gui.get_node(node .. "/check")
@@ -25,21 +25,45 @@ function M.checkbox(self, action_id, action, node, enabled, text)
 
 	-- Check current value
 	self.checkbox = self.checkbox or {}
-	self.selectedNode = D.nodes["active"] or nil
-
-	if enabled then
-		if self.checkbox[node] then
-			gui.set_color(bgNode, D.colors.active)
-		else
-			gui.set_color(bgNode, D.colors.accent)
+	self.checkbox[node] = self.checkbox[node] or {}
+	self.checkbox[node].init = self.checkbox[node].init or false
+	self.checkbox[node].value = self.checkbox[node].value or false
+	self.checkbox[node].enabled = enabled 
+	D.nodes["active"] = D.nodes["active"] or nil
+	
+	if not self.checkbox[node].init and enabled then
+		-- Set standrad value
+		if standard_value ~= nil then
+			self.checkbox[node].value = standard_value
 		end
+		-- Update
+		if self.checkbox[node].value then
+			gui.set_enabled(checkNode, true)
+			gui.set_color(bgNode, D.colors.accent)
+		else
+			gui.set_enabled(checkNode, false)
+			gui.set_color(bgNode, D.colors.active)
+		end
+		self.checkbox[node].init = true	
+	elseif enabled then
+		if self.checkbox[node].value then
+			gui.set_color(bgNode, D.colors.accent)
+			gui.set_enabled(checkNode, true)
+		else
+			gui.set_color(bgNode, D.colors.active)
+			gui.set_enabled(checkNode, false)
+		end
+	else
+		self.checkbox[node].value = false
+		gui.set_enabled(checkNode, false)
+		gui.set_color(bgNode, D.colors.inactive)
 	end
 	
 	-- Check if hovering above
-	if gui.pick_node(bgNode, action.x, action.y) and enabled and (self.selectedNode == nil or self.selectedNode == node) then
+	if gui.pick_node(bgNode, action.x, action.y) and enabled and (D.nodes["active"]== nil or D.nodes["active"] == node) then
 		-- Set as active node
 		D.nodes["active"] = node
-		if self.checkbox[node] then
+		if self.checkbox[node].value then
 			gui.set_color(bgNode, D.colors.accenthover)
 		else
 			gui.set_color(bgNode, D.colors.hover)
@@ -53,20 +77,20 @@ function M.checkbox(self, action_id, action, node, enabled, text)
 			gui.set_enabled(txtBox, true)
 		end
 		-- When pressed check if to be activated or deactivated
-		if action_id == hash("touch") and action.pressed and self.checkbox[node] then
-			self.checkbox[node] = false
+		if action_id == hash("touch") and action.pressed and self.checkbox[node].value then
+			self.checkbox[node].value = false
 			gui.set_enabled(checkNode, false)
 			gui.set_color(bgNode, D.colors.hover)
-		elseif action_id == hash("touch") and action.pressed and self.checkbox[node] ~= true then
-			self.checkbox[node] = true
+		elseif action_id == hash("touch") and action.pressed and self.checkbox[node].value ~= true then
+			self.checkbox[node].value = true
 			gui.set_enabled(checkNode, true)
 			gui.set_color(bgNode, D.colors.accenthover)
 		end
-	elseif enabled and not gui.pick_node(bgNode, action.x, action.y) and self.selectedNode == node then
+	elseif enabled and not gui.pick_node(bgNode, action.x, action.y) and D.nodes["active"] == node then
 		gui.set_enabled(txtBox, false)
 		gui.set_color(bgNode, D.colors.active)
 		D.nodes["active"] = nil
-		if self.checkbox[node] then
+		if self.checkbox[node].value then
 			gui.set_color(bgNode, D.colors.accent)
 		else
 			gui.set_color(bgNode, D.colors.active)
@@ -79,10 +103,10 @@ function M.checkbox(self, action_id, action, node, enabled, text)
 		gui.set_enabled(txtBox, false)
 	end
 	--return value
-	return self.checkbox[node]
+	return self.checkbox[node].value
 end
 
-function M.checkboxSelectall(self, action_id, action, node, othernodes, enabled, text)
+function M.checkboxSelectall(self, action_id, action, node, othernodes, enabled, standard_value, text)
 	-- Load nodes
 	local bgNode = gui.get_node(node .. "/bg")
 	local checkNode = gui.get_node(node .. "/check")
@@ -91,96 +115,134 @@ function M.checkboxSelectall(self, action_id, action, node, othernodes, enabled,
 
 	-- Check current value
 	self.checkbox = self.checkbox or {}
-	self.selectedNode = D.nodes["active"] or nil
+	self.checkbox[node] = self.checkbox[node] or {}
+	self.checkbox[node].init = self.checkbox[node].init or false
+	self.checkbox[node].value = self.checkbox[node].value or false
+	self.checkbox[node].enabled = enabled
+	D.nodes["active"] = D.nodes["active"] or nil
 
-	if enabled then
+	-- Initalize
+	if not self.checkbox[node].init and enabled then
+		-- Set standrad value
+		if standard_value ~= nil then
+			self.checkbox[node].value = standard_value
+		end
+		-- Update
+		if self.checkbox[node].value then
+			gui.set_enabled(checkNode, true)
+			gui.set_color(bgNode, D.colors.accent)
+			for i = 1, #othernodes do
+				if self.checkbox[othernodes[i]].enabled then
+					local otherBgNode = gui.get_node(othernodes[i] .. "/bg")
+					local otherCheckNode = gui.get_node(othernodes[i] .. "/check")
+					self.checkbox[othernodes[i]].value = true
+					gui.set_enabled(otherCheckNode, true)
+					gui.set_color(otherBgNode, D.colors.accent)
+				else
+					local otherBgNode = gui.get_node(othernodes[i] .. "/bg")
+					local otherCheckNode = gui.get_node(othernodes[i] .. "/check")
+					self.checkbox[othernodes[i]].value = false
+					gui.set_enabled(otherCheckNode, false)
+					gui.set_color(otherBgNode, D.colors.inactive)
+				end
+			end
+		else
+			self.checkbox[node].value = false
+			gui.set_enabled(checkNode, false)
+			gui.set_color(bgNode, D.colors.inactive)
+		end
+		self.checkbox[node].init = true
+	end
+
+	-- Run input
+	if enabled and self.checkbox[node].init then
 		-- Check other checkboxes
 		local numberActive = 0
-		for i = 0, #othernodes do
-			if self.checkbox[othernodes[i]] then
+		for i = 1, #othernodes do
+			if self.checkbox[othernodes[i]].value then
 				numberActive = numberActive + 1
 			end
 		end
 		if numberActive > 0 and numberActive < #othernodes then
 			gui.set_enabled(checkNode, true)
 			gui.play_flipbook(checkNode, "line")
-			self.checkbox[node] = true
+			self.checkbox[node].value = true
 			gui.set_color(bgNode, D.colors.accent)
 		elseif numberActive == #othernodes then
 			gui.set_enabled(checkNode, true)
 			gui.play_flipbook(checkNode, "check")
-			self.checkbox[node] = true
+			self.checkbox[node].value = true
 			gui.set_color(bgNode, D.colors.accent)
 		else
 			gui.set_enabled(checkNode, false)
-			self.checkbox[node] = false
+			self.checkbox[node].value = false
 			gui.set_color(bgNode, D.colors.active)
 		end
+	end
 		
-		-- Check if hovering above
-		if gui.pick_node(bgNode, action.x, action.y) and (self.selectedNode == nil or self.selectedNode == node) then
-			-- Set as active node
-			D.nodes["active"] = node
-			if self.checkbox[node] then
-				gui.set_color(bgNode, D.colors.accenthover)
-			else
-				gui.set_color(bgNode, D.colors.hover)
+	-- Check if hovering above
+	if gui.pick_node(bgNode, action.x, action.y) and (D.nodes["active"]  == nil or D.nodes["active"]  == node) and enabled then
+		-- Set as active node
+		D.nodes["active"] = node
+		if self.checkbox[node].value then
+			gui.set_color(bgNode, D.colors.accenthover)
+		else
+			gui.set_color(bgNode, D.colors.hover)
+		end
+
+		-- Set text to explination box and adjust size
+		if text ~= nil then
+			gui.set_text(txtNode, text)
+			local text_width = gui.get_text_metrics_from_node(txtNode).width
+			local current_size = gui.get_size(txtBox)
+			gui.set_size(txtBox, vmath.vector3(text_width + 20, current_size.y, current_size.z))
+			gui.set_enabled(txtBox, true)
+		end
+
+		-- When pressed check if to be activated or deactivated
+		if action_id == hash("touch") and action.pressed and self.checkbox[node].value then
+			self.checkbox[node].value = false
+			gui.set_enabled(checkNode, false)
+			gui.set_color(bgNode, D.colors.hover)
+			-- Deactivate all other
+			for i = 1, #othernodes do
+				local otherBgNode = gui.get_node(othernodes[i] .. "/bg")
+				local otherCheckNode = gui.get_node(othernodes[i] .. "/check")
+				self.checkbox[othernodes[i]].value = false
+				gui.set_enabled(otherCheckNode, false)
+				gui.set_color(otherBgNode, D.colors.active)
 			end
-			
-			-- Set text to explination box and adjust size
-			if text ~= nil then
-				gui.set_text(txtNode, text)
-				local text_width = gui.get_text_metrics_from_node(txtNode).width
-				local current_size = gui.get_size(txtBox)
-				gui.set_size(txtBox, vmath.vector3(text_width + 20, current_size.y, current_size.z))
-				gui.set_enabled(txtBox, true)
+		elseif action_id == hash("touch") and action.pressed and self.checkbox[node].value ~= true then
+			self.checkbox[node].value = true
+			gui.set_enabled(checkNode, true)
+			gui.set_color(bgNode, D.colors.accenthover)
+			gui.play_flipbook(checkNode, "check")
+			for i = 1, #othernodes do
+				local otherBgNode = gui.get_node(othernodes[i] .. "/bg")
+				local otherCheckNode = gui.get_node(othernodes[i] .. "/check")
+				self.checkbox[othernodes[i]].value = true
+				gui.set_enabled(otherCheckNode, true)
+				gui.set_color(otherBgNode, D.colors.accent)
 			end
-			
-			-- When pressed check if to be activated or deactivated
-			if action_id == hash("touch") and action.pressed and self.checkbox[node] then
-				self.checkbox[node] = false
-				gui.set_enabled(checkNode, false)
-				gui.set_color(bgNode, D.colors.hover)
-				-- Deactivate all other
-				for i = 1, #othernodes do
-					local otherBgNode = gui.get_node(othernodes[i] .. "/bg")
-					local otherCheckNode = gui.get_node(othernodes[i] .. "/check")
-					self.checkbox[othernodes[i]] = false
-					gui.set_enabled(otherCheckNode, false)
-					gui.set_color(otherBgNode, D.colors.active)
-				end
-			elseif action_id == hash("touch") and action.pressed and self.checkbox[node] ~= true then
-				self.checkbox[node] = true
-				gui.set_enabled(checkNode, true)
-				gui.set_color(bgNode, D.colors.accenthover)
-				gui.play_flipbook(checkNode, "check")
-				for i = 1, #othernodes do
-					local otherBgNode = gui.get_node(othernodes[i] .. "/bg")
-					local otherCheckNode = gui.get_node(othernodes[i] .. "/check")
-					self.checkbox[othernodes[i]] = true
-					gui.set_enabled(otherCheckNode, true)
-					gui.set_color(otherBgNode, D.colors.accent)
-				end
-			end
-		elseif enabled and not gui.pick_node(bgNode, action.x, action.y) and self.selectedNode == node then
-			gui.set_enabled(txtBox, false)
+		end
+	elseif enabled and not gui.pick_node(bgNode, action.x, action.y) and D.nodes["active"] == node then
+		gui.set_enabled(txtBox, false)
+		gui.set_color(bgNode, D.colors.active)
+		D.nodes["active"] = nil
+		if self.checkbox[node].value then
+			gui.set_color(bgNode, D.colors.accent)
+		else
 			gui.set_color(bgNode, D.colors.active)
-			D.nodes["active"] = nil
-			if self.checkbox[node] then
-				gui.set_color(bgNode, D.colors.accent)
-			else
-				gui.set_color(bgNode, D.colors.active)
-			end
 		end
 	elseif enabled == false then
 		gui.set_color(bgNode, D.colors.inactive)
-		if self.selectedNode == node then
+		if D.nodes["active"]  == node then
 			D.nodes["active"] = nil
 		end
 		gui.set_enabled(txtBox, false)
 	end
 	--return value
-	return self.checkbox[node]
+	return self.checkbox[node].value
 end
 
 return M
